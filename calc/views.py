8dea98d23fd24c06b0eccf19c6268f47ac994c2a
Sandra Cantero Rabanillo <sdkr@zeta40.aulas.gsyc.es>
2016-04-06 19:04:09 +0200

    Ejercicio resuelto

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def suma(request, num1, num2):
+    respuesta = int(num1) + int(num2)
+    return HttpResponse(respuesta)
+
+def resta(request, num1, num2):
+    respuesta = int(num1) - int(num2)
+    return HttpResponse(respuesta)
+
+def multiplicacion(request, num1, num2):
+    respuesta = int(num1) * int(num2)
+    return HttpResponse(respuesta)
+
+def division(request, num1, num2):
+    try:
+        respuesta = int(num1) / int(num2)
+    except ZeroDivisionError:
+        respuesta = "Imposible dividir por 0"
+    return HttpResponse(respuesta)
+
+def notfound(request):
+    respuesta = "<br>El formato correcto es:"
+    respuesta += "<li>num1+num2"
+    respuesta += "<li>num1-num2"
+    respuesta += "<li>num1*num2"
+    respuesta += "<li>num1/num2 donde num2 es distinto de 0</li>"
+    respuesta += "<br>Los numeros pueden ser positivos o negativos, "
+    respuesta += "basta con poner un signo menos delante (sin parentesis)"
+    respuesta += "<br>Y solo se devuelven numeros enteros"
+    return HttpResponse(respuesta)
