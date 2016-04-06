9ac839818b3414caa94c7367d6744bcf60db5158
Sandra Cantero Rabanillo <sdkr@zeta40.aulas.gsyc.es>
2016-04-06 19:03:30 +0200

    Ejercicio resuelto

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^(\-?\d+)\+(\-?\d+)$', 'calc.views.suma'),
+    url(r'^(\-?\d+)\-(\-?\d+)$', 'calc.views.resta'),
+    url(r'^(\-?\d+)\*(\-?\d+)$', 'calc.views.multiplicacion'),
+    url(r'^(\-?\d+)\/(\-?\d+)$', 'calc.views.division'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', 'calc.views.notfound'),
 )
